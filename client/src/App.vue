ab9eb00743dd40a7a153131ee524f4f74dc58345
Jackson Patrício de Morais <jackson.dmor@gmail.com>
2019-10-28 19:49:11 -0300

    organizacao de arquivo CSS

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,6 +10,7 @@
 
 <script>
 import "bootstrap/dist/css/bootstrap.css"
+import "../content/style.css"
 import AppFiltros from "./components/AppFiltros"
 
 export default {
@@ -21,90 +22,4 @@ export default {
 </script>
 
 <style>
-* {
-  padding: 0;
-  margin: 0;
-}
-
-body {
-  display: block;
-}
-
-.cabecalho {
-  background: #fff;
-  color: #333;
-  padding: 50px 0 10px 30px;
-}
-
-.filtros {
-  background: lightgray;
-  color: #333;
-  padding: 30px;
-  border: lightgray solid 1px;
-  margin: 0 30px
-}
-
-.filtros button {
-  margin-top: 15px;
-  min-width: 150px;
-}
-
-.conteudo {
-  background: #fff;
-  color: #333;
-  padding: 0;
-  margin: 0 30px
-}
-
-.conteudo-grid {
-  background: #fff;
-  color: #333;
-  padding: 0 80px;
-  margin: 0 30px;
-}
-
-table {
-  border-radius: 3px;
-  background-color: #fff;
-  width: 100%;
-}
-
-th {
-  background-color: lightgray;
-  color: #333;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-
-td {
-  background-color: #f9f9f9;
-}
-
-th, td {
-  min-width: 120px;
-  padding: 10px 20px;
-  border: #fff solid 1px;
-}
-
-tbody tr:hover {
-  background-color: lightskyblue;
-}
-
-.warming h6 {
-  color: crimson;
-  margin: 0;
-}
-
-#layout {
-  margin: 10px 30px 10px 0
-}
-
-.rodape {
-  background: #fff;
-  color: #333;
-  padding: 30px;
-  margin: 0 30px
-}
 </style>
