ef6f02601e262f7450c63c9b0dd0b13c67e2325c
Jackson Patrício de Morais <jackson.dmor@gmail.com>
2019-10-28 14:06:51 -0300

    colocado validacao em filtros e feedbacks para usuario

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,24 +8,30 @@
       <div class="row">
         <div class="col-md">
           <h5>Tag</h5>
-          <input class="form-control" type="text" v-model="tag">
+          <input class="form-control" type="text" v-model="tag" autofocus>
         </div>
         <div class="col-md">
           <h5>Limit</h5>
-          <input class="form-control" type="text" v-model="limit">
+          <input class="form-control" type="text" v-model="limit" v-on:keydown="numberValidation">
         </div>
         <div class="col-md">
           <h5>Score</h5>
-          <input class="form-control" type="text" v-model="score">
+          <input class="form-control" type="text" v-model="score" v-on:keydown="numberValidation">
         </div>
         <div class="col-md">
           <h5>Sort</h5>
-          {{ sort }}
-          <input class="form-control" type="text" v-model="sort"> 
+          <select class="form-control" v-model="sort">
+            <option>Activity</option>
+            <option>Creation</option>
+            <option>Votes</option>
+          </select>
         </div>
       </div>
       <div class="text-right">
-        <button class="btn btn-success" v-on:click="getQuestions(tag, limit, score, sort)">Buscar</button>
+        <button class="btn btn-success" v-on:click="getQuestions(tag || '-', limit || '-', score || '-', sort)">Buscar</button>
+      </div>
+      <div class="warming">
+        <h6>{{ warning }}</h6>
       </div>
     </nav>
     <main class="conteudo">
@@ -68,12 +74,31 @@ export default {
       limit: "", 
       score: "", 
       sort: "",
+      warning: "",
       rowData:[]
     }
   },
   methods: {
-    getQuestions(tag, limit, score, sort) {
-      const where = `tag: "${tag}", limit: "${limit}", score: "${score}", sort: "${sort}"`
+    getQuestions(tag, limit, score, sort) {           
+      if (tag === '-') {
+        this.warning = "Campo TAG é obrigatório"
+        return
+      }
+      
+      let params = [`tag: "${tag}"`]
+
+      if (limit != '-'){
+        params.push(`limit: ${limit}`)
+      }
+
+      if (score != '-'){
+        params.push(`score: ${score}`)
+      }
+
+      params.push(`sort: "${sort}"`)
+
+      let where = params.join(',')
+
       axios({
         url: "http://localhost:4000",
         method: "post",
@@ -98,14 +123,35 @@ export default {
         this.rowData = []
         const query = response.data
         Array.from(query.data.getQuestions).forEach(questions => this.rowData.push(questions))
+
+        if (this.rowData.length === 0) {
+          this.warning = "Nenhum registro encontrado na busca!"
+        } else {
+          this.warning = ""
+        }        
         /* eslint-enable no-console */
       }).catch(error => {
         if (error) {
           /* eslint-disable no-console */
-          return console.log(error)
+          this.warning = error
           /* eslint-enable no-console */
         }
       })
+    },
+    numberValidation(e) {
+      let charCode = e.charCode ? e.charCode : e.keyCode;
+      // charCode 8 = backspace
+      // charCode 9 = tab
+      if (charCode != 8 && charCode != 9) {
+          // charCode 48 equivale a 0
+          // charCode 57 equivale a 9
+          if (((charCode < 48 || charCode > 57) && (charCode < 96 || charCode > 105)) && charCode !== 46) {                
+            this.warning = "Campo apenas numérico"
+            e.preventDefault()               
+          } else {            
+            this.warning = "" 
+          }
+      }
     }
   }
 }
@@ -174,6 +220,11 @@ th, td {
 
 tbody tr:hover {
   background-color: lightskyblue;
+}
+
+.warming h6 {
+  color: crimson;
+  margin: 0;
 }
 
 .rodape {
